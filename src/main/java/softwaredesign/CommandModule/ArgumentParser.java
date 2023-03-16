24fa7072f209dd6153aad58efc068e81a8e2400b
marko <markovla2001@gmail.com>
2023-03-16 15:08:31 +0100

    Declared Command and ArgumentParser classes.

---
+++ src/main/java/softwaredesign/CommandModule/ArgumentParser.java
@@ -0,0 +1,13 @@
+package softwaredesign.CommandModule;
+
+/**
+ * @author Marko
+ *
+ * This class is going to parse the input arguments, in a Command object.
+ * <p>
+ *     It will provide a "Map<String,String>" with pre-defined argument keys ("name", "sort-by") and argument values.
+ * </p>
+ */
+public class ArgumentParser {
+
+}
---
+++ src/main/java/softwaredesign/CommandModule/Command.java
@@ -0,0 +1,27 @@
+package softwaredesign.CommandModule;
+
+/**
+ * @author Marko
+ *
+ * This abstract class provides a template for the actual
+ * command classes (StatisticCommand, DeleteRepositoryCommand, SwitchBranchCommand)
+ *
+ */
+public abstract class Command {
+
+}
+
+class StatisticCommand extends Command {
+
+
+}
+
+class DeleteRepositoryCommand extends Command {
+
+
+}
+
+class SwitchBranchCommand extends Command {
+
+
+}
