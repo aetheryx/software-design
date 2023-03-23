a6d2dde2a8ff7382028bd8abe8ff5be3a951977c
marko <markovla2001@gmail.com>
2023-03-23 11:58:42 +0100

    Started addArgument() and parse() implementation

--- src/main/java/softwaredesign/CommandModule/ArgumentParser.java
+++ src/main/java/softwaredesign/CommandModule/ArgumentParser.java
@@ -1,9 +1,11 @@
 package softwaredesign.CommandModule;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
- * @author Marko
+ *
  *
  * This class is going to parse the input arguments, in a Command object.
  *
@@ -13,15 +15,28 @@ import java.util.Map;
  *     which it is going to parse the parsed key's value (String[]) and
  *     search if the argument key's value is found in it or not.
  * </p>
+ *
+ * @author Marko
  */
 public class ArgumentParser {
     private Map<String, String[]> arguments;
 
     ArgumentParser addArgument(String name, String[] values){
+
+        if(!arguments.containsKey(name)){
+            arguments.put(name, values);
+        }
+        else {
+            // TODO: It should return an error or overwrite?
+        }
+
         return this;
     }
 
     Map<String, String[]> parse (String input){
+        ArrayList<String> keys = new ArrayList<>();
+        ArrayList<String> values = new ArrayList<>();
+
         return null;
     }
 
