f926c292ee752bd77b925b0c5e3ff408812aa863
mhzyali <m.z.y.ali@student.vu.nl>
2023-03-26 08:56:25 +0200

    Implement Framework.Module for the Statistic class

--- src/main/java/softwaredesign/StatisticsModule/Statistic.java
+++ src/main/java/softwaredesign/StatisticsModule/Statistic.java
@@ -1,21 +1,26 @@
 package softwaredesign.StatisticsModule;
 
+import softwaredesign.CommandModule.StatisticCommand;
+import softwaredesign.Util.Framework;
+
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This interface represents a statistic that can be calculated on a GitHub repository.
- * It contains a name property, which is the name of the statistic (e.g. “most-active-contributors”), which the
- * StatisticCommand uses to select this statistic based on the user’s input string.
- * The calculation of the statistic happens in the <a href="#@link">{@link Statistic#calculate(Map)}</a> method.
+ * Represents a statistic that can be calculated on a GitHub repository.
+ * Contains a name property, which is the name of the statistic (e.g. “most-active-contributors”), which the
+ * {@link StatisticCommand} uses to select this statistic based on the user’s input string.
+ * The calculation of the statistic happens in the {@link Statistic#calculate(Map)} method.
  *
+ * Implements the {@link Framework.Module} interface, because instances of the Statistic class are
+ * modules in the {@link StatisticFramework} class used by the {@link Statistic} command.
  * @author Zain
  */
-public abstract class Statistic {
+public abstract class Statistic implements Framework.Module {
     private String name;
 
     public Statistic(String name) {
         this.name = name;
-        StatisticsManager.registerStatistic(this);
     }
 
     public String getName() {
