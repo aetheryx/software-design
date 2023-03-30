73467fb6db46b1dfdd5f3aecba9fb3603f57c505
joachim69 <Joachimbose48@gmail.com>
2023-03-30 23:58:10 +0200

    worked on my statistic

--- src/main/java/softwaredesign/StatisticsModule/ContributorStatistic.java
+++ src/main/java/softwaredesign/StatisticsModule/ContributorStatistic.java
@@ -1,7 +1,12 @@
 package softwaredesign.StatisticsModule;
 
-import java.util.Comparator;
-import java.util.Map;
+import softwaredesign.RepositoryModule.Commit;
+import softwaredesign.UI.ProgressBar;
+import softwaredesign.UI.Table;
+
+import javax.swing.plaf.IconUIResource;
+import java.util.*;
+
 /**
  * <p>
  *     This class is responsible for calculating a ranking for every contributor based on contribution. This ranking
@@ -16,10 +21,8 @@ import java.util.Map;
  * </p>
  * */
 public class ContributorStatistic extends GitStatistic {
-    public ContributorStatistic() {
-        super("contributors");
-    }
-
+    private static final String STATISTIC_CALCULATING_TASK_NAME = "(Step 1/2): calculating most active contributors...";
+    private static final String RESULT_GENERATING_TASK_NAME = "(Step 2/2): generating results...";
 
     private class Contributor{
         private int linesOfCodeChanged;
@@ -31,7 +34,7 @@ public class ContributorStatistic extends GitStatistic {
                 return contributor1.linesOfCodeChanged - secondContributor.linesOfCodeChanged;
             }
         }
-        private class CommitsComparator implements Comparator<Contributor>{
+        public class CommitsComparator implements Comparator<Contributor>{
             @Override
             public int compare(Contributor contributor1, Contributor secondContributor) {
                 return contributor1.nCommits - secondContributor.nCommits;
@@ -48,10 +51,39 @@ public class ContributorStatistic extends GitStatistic {
         public int getnCommits() {
             return nCommits;
         }
-
     }
     @Override
     public void calculate(Map<String, String> arguments) {
-        // todo
+        List<Commit> commits = repository.getCommits();
+        ProgressBar progressBar = new ProgressBar(STATISTIC_CALCULATING_TASK_NAME, 20); //TODO: how long should this bar be?
+        progressBar.start();
+        Map<String, Contributor> contributors = new HashMap<>();
+        Table table = new Table(new String[]{"Author name", "number of lines changed", "number of commits"});
+
+        for (int i = 0; i < commits.size(); i++) {
+            Commit commitToProcess = commits.get(i);
+            progressBar.setProgress((float) i / (float)commits.size()); //this updates the progressbar on screen
+            if (!contributors.containsKey(commitToProcess.getAuthor())){
+                contributors.put(commitToProcess.getAuthor(), new Contributor(commitToProcess.getDiffAdded(), 1));
+            }
+            else {
+                contributors.get(commitToProcess.getAuthor()).nCommits ++;
+                contributors.get(commitToProcess.getAuthor()).linesOfCodeChanged += commitToProcess.getDiffAdded();
+            }
+        }
+        progressBar.finish("success");
+
+        progressBar = new ProgressBar(RESULT_GENERATING_TASK_NAME, 20); //TODO: how long should this bar be?
+        List<Contributor> contributorList = new ArrayList<>(contributors.values());
+        contributorList.sort(new Contributor.CommitsComparator());
+        for (int i = 0; i < contributors.size(); i++) {
+            progressBar.setProgress((float) i / (float)contributors.size());
+            table.addEntry(); //TODO: add arguments
+        }
+        progressBar.finish(table.toString());
+    }
+
+    public ContributorStatistic() {
+        super("contributors");
     }
 }
