906995f4c593d0af059a913f2d97961c29a0090c
mhzyali <m.z.y.ali@student.vu.nl>
2023-03-31 17:02:06 +0200

    implement weekday statistic

--- src/main/java/softwaredesign/StatisticsModule/WeekdayStatistic.java
+++ src/main/java/softwaredesign/StatisticsModule/WeekdayStatistic.java
@@ -3,6 +3,7 @@ package softwaredesign.StatisticsModule;
 import softwaredesign.UI.Table;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WeekdayStatistic extends GitStatistic {
     public WeekdayStatistic() {
@@ -11,7 +12,19 @@ public class WeekdayStatistic extends GitStatistic {
 
     @Override
     public Table calculate(Map<String, String> arguments) {
-        // todo
-        return null;
+        Table table = new Table("Weekday", "commits", "loc");
+
+        repository.getCommits()
+                .stream()
+                .collect(Collectors.groupingBy(c -> c.getDate().getDayOfWeek()))
+                .forEach((weekday, commits) -> {
+                    int numLoc = commits.stream()
+                            .map(c -> c.getDiffAdded() + c.getDiffRemoved())
+                            .reduce(0, Integer::sum);
+
+                    table.addEntry(weekday.name(), commits.size(), numLoc);
+                });
+
+        return table;
     }
 }
